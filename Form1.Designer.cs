5f98ecfdd85fd884d59fbe4ae566144af3798fc9
BrayanD07 <52518417+BrayanD07@users.noreply.github.com>
2020-05-25 03:38:25 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Numeros
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnIngresar_datos = new System.Windows.Forms.Button();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.btnOrdenar = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.txtIntroduce = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(352, 30);
+            this.listBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(72, 180);
+            this.listBox3.TabIndex = 23;
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Location = new System.Drawing.Point(365, 9);
+            this.lbl4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(59, 17);
+            this.lbl4.TabIndex = 22;
+            this.lbl4.Text = "Al revez";
+            this.lbl4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(460, 30);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(72, 180);
+            this.listBox2.TabIndex = 21;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(577, 30);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(72, 180);
+            this.listBox1.TabIndex = 20;
+            // 
+            // btnIngresar_datos
+            // 
+            this.btnIngresar_datos.Location = new System.Drawing.Point(121, 46);
+            this.btnIngresar_datos.Margin = new System.Windows.Forms.Padding(4);
+            this.btnIngresar_datos.Name = "btnIngresar_datos";
+            this.btnIngresar_datos.Size = new System.Drawing.Size(100, 28);
+            this.btnIngresar_datos.TabIndex = 19;
+            this.btnIngresar_datos.Text = "Ingresar ";
+            this.btnIngresar_datos.UseVisualStyleBackColor = true;
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(447, 9);
+            this.lbl3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(103, 17);
+            this.lbl3.TabIndex = 18;
+            this.lbl3.Text = "Menor a mayor";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(558, 9);
+            this.lbl2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(103, 17);
+            this.lbl2.TabIndex = 17;
+            this.lbl2.Text = "Mayor a Menor";
+            // 
+            // btnOrdenar
+            // 
+            this.btnOrdenar.Location = new System.Drawing.Point(93, 147);
+            this.btnOrdenar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnOrdenar.Name = "btnOrdenar";
+            this.btnOrdenar.Size = new System.Drawing.Size(148, 31);
+            this.btnOrdenar.TabIndex = 16;
+            this.btnOrdenar.Text = "ordenar";
+            this.btnOrdenar.UseVisualStyleBackColor = true;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(100, 9);
+            this.lbl1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(152, 17);
+            this.lbl1.TabIndex = 15;
+            this.lbl1.Text = "Introduce los números:";
+            // 
+            // txtIntroduce
+            // 
+            this.txtIntroduce.Location = new System.Drawing.Point(12, 82);
+            this.txtIntroduce.Margin = new System.Windows.Forms.Padding(4);
+            this.txtIntroduce.Name = "txtIntroduce";
+            this.txtIntroduce.Size = new System.Drawing.Size(276, 22);
+            this.txtIntroduce.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 226);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnIngresar_datos);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.btnOrdenar);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.txtIntroduce);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnIngresar_datos;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Button btnOrdenar;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.TextBox txtIntroduce;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Collections;
+
+namespace Numeros
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ArrayList list = new ArrayList();
+        Thread hilo1;
+        Thread hilo2;
+        Thread hilo3;
+        
+
+        public void btnIngresar_datos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void MyrMnr(int[] vector)
+        {
+            
+            
+            int n = vector.Length - 1;
+            int c;
+            int aux = 0;
+
+            for (int a = 0; a <= n; a++)
+            {
+                for (int b = 0; b <= n; b++)
+                {
+                    if (b + 1 < n || b + 1 == n)
+                    {
+                        if (vector[b] < vector[b + 1])
+                        {
+                            aux = vector[b];
+                            vector[b] = vector[b + 1];
+                            vector[b + 1] = aux;
+                        }
+                    }
+                }
+            }
+            listBox1.Items.Clear();
+            for (c = 0; c <= n; c++)
+            {
+                listBox1.Items.Add(vector[c].ToString());
+            }
+
+        }
+
+        public void MnrMyr(int[] vector)
+        {
+           
+            int n = vector.Length - 1;
+            int c;
+            int aux = 0;
+
+            for (int a = 0; a <= n; a++)
+            {
+                for (int b = 0; b <= n; b++)
+                {
+                    if (b + 1 < n || b + 1 == n)
+                    {
+                        if (vector[b] > vector[b + 1])
+                        {
+                            aux = vector[b];
+                            vector[b] = vector[b + 1];
+                            vector[b + 1] = aux;
+                        }
+                    }
+                }
+            }
+            listBox2.Items.Clear();
+            for (c = 0; c <= n; c++)
+            {
+                listBox2.Items.Add(vector[c].ToString());
+            }
+        }
+
+        public void revez(int[] vector)
+        {
+           
+
+            int n = vector.Length - 1;
+            int c = 0;
+            for (int a = 0; a < n; a++)
+            {
+                c = vector[a];
+                vector[a] = vector[a + 1];
+                vector[a + 1] = c;
+            }
+
+
+            listBox3.Items.Clear();
+            for (c = 0; c <= n; c++)
+            {
+                listBox3.Items.Add(vector[c].ToString());
+            }
+            c++;
+        }
+
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        {
+            int[] vector = (int[])list.ToArray(typeof(int));
+            object[] vector2 = new object[vector.Length];
+
+            
+
+            //MnrMyr(vector);
+            revez(vector);
+            MyrMnr(vector);
+            Thread a = new Thread(MnrMyr(vector));
+            a.Start();
+        }
+        
+    }
+}
